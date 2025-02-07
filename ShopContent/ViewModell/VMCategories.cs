b9cfd3bed9a6d505985da7fd7af3a39fb86633c3
paranoe <is-21-4-6@permaviat.ru>
2025-02-07 17:59:13 +0500

    Разработанные модели представления. Дружинин

---
+++ ShopContent/ViewModell/VMCategories.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ShopContent.ViewModell
+{
+    public class VMCategories : INotifyPropertyChanged
+    {
+        public ObservableCollection<Context.CategoriesContext> Categories { get; set; }
+        public VMCategories() => Categories = Context.CategoriesContext.AllCategories();
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public Classes.RelayCommand NewCategories
+        {
+            get
+            {
+                return new Classes.RelayCommand(obj =>
+                {
+                    Context.CategoriesContext newModell = new Context.CategoriesContext(true);
+                    Categories.Add(newModell);
+                    MainWindow.Instance.frame.Navigate(new View.Categories.Add(newModell));
+                }
+                );
+            }
+        }
+    }
+}
